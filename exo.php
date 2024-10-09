c26cbaaa7ee4e4717a6e5718bd0e64f7202a76bd
SANGARALINGAM LAKSHAN <lakshan.sangaralingam@lycee-jeanrostand.fr>
2024-10-09 16:26:02 +0200

    exo 12

--- exo.php
+++ exo.php
@@ -1,10 +1,6 @@
 <?php
-function plusPetit($arg1,$arg2){
-    if ($arg1<$arg2){
-        return $arg1;
-    }else {
-        return $arg2;
+function plusPetit($arg1,$arg2,$arg3){
+    return min($arg1,$arg2,$arg3);
     }
-
-}
+echo plusPetit(9,54,1);
 ?>
