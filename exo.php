960529cf23cffbc70e261d837fdb26a8fc8169ee
SANGARALINGAM LAKSHAN <lakshan.sangaralingam@lycee-jeanrostand.fr>
2024-10-09 16:47:58 +0200

    exo 18

--- exo.php
+++ exo.php
@@ -1,11 +1,14 @@
 <?php
+
 function verificationPassword($arg){
-    if (strlen($arg)>=8){
-        return true ;
-    } else {
+    if (strlen($arg)>=8 && strpbrk($arg, '0123456789') && strpbrk($arg, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ')  && 
+    strpbrk($arg, 'abcdefghijklmnopqrstuvwxyz')  ){
+        return true;
+    }else{
         return false;
     }
-
 }
-echo verificationPassword(lakzduia)
+echo verificationPassword(lakshan)
+
 ?>
+
