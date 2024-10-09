638e1b057ddd7e5a14f33b6167fe25e029296693
SANGARALINGAM LAKSHAN <lakshan.sangaralingam@lycee-jeanrostand.fr>
2024-10-09 16:51:03 +0200

    exo 19

--- exo.php
+++ exo.php
@@ -1,14 +1,27 @@
 <?php
-
-function verificationPassword($arg){
-    if (strlen($arg)>=8 && strpbrk($arg, '0123456789') && strpbrk($arg, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ')  && 
-    strpbrk($arg, 'abcdefghijklmnopqrstuvwxyz')  ){
-        return true;
-    }else{
-        return false;
+function capital($pays) {
+    switch ($pays) {
+        case 'France':
+            return 'Paris';
+        case 'Allemagne':
+            return 'Berlin';
+        case 'Italie':
+            return 'Rome';
+        case 'Maroc':
+            return 'Rabat';
+        case 'Espagne':
+            return 'Madrid';
+        case 'Portugal':
+            return 'Lisbonne';
+        case 'Angleterre':
+            return 'Londres';
+        default:
+            return 'Inconnu';
     }
 }
-echo verificationPassword(lakshan)
 
+echo capital('France');   
+echo capital('Allemagne');  
+echo capital('Italie');     
+echo capital('Canada');     
 ?>
-
