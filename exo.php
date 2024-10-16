f2d82add8e626e1308ce8e417981bd8d9b4e2db5
SANGARALINGAM LAKSHAN <lakshan.sangaralingam@lycee-jeanrostand.fr>
2024-10-16 15:29:26 +0200

    exo 22

--- exo.php
+++ exo.php
@@ -1,12 +1,8 @@
 <?php
-function remplacerLesLettres($chaine) {
-    $chaine = str_replace('e', '3', $chaine);
-    $chaine = str_replace('i', '1', $chaine);
-    $chaine = str_replace('o', '0', $chaine);
-    return $chaine;
+function quelleAnnee() {
+    return (int)date("Y");
 }
 
-echo remplacerLesLettres("Bonjour les amis") . "\n";
-echo remplacerLesLettres("Les cours de programmation Web sont trops cools");
+echo quelleAnnee();
 
 ?>
