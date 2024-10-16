67980b7f7b8855569b3356068cd33f2b778ba6e6
SANGARALINGAM LAKSHAN <lakshan.sangaralingam@lycee-jeanrostand.fr>
2024-10-16 15:38:29 +0200

    exo 24

--- exo.php
+++ exo.php
@@ -1,11 +1,11 @@
 <?php
-function quelleDate() {
-    $jour = (int)date("d");
-    $mois = (int)date("m");
-    $annee = (int)date("Y");
-    return sprintf("%02d/%02d/%04d", $jour, $mois, $annee);
+include './libraryToInclude.php';
+
+function fonctionMagique() {
+    return fonctionDeMaLibrairie();
 }
 
-echo quelleDate();
+
+echo fonctionMagique();
 
 ?>
